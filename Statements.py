61f12067dde657c68c9aa25579789385ab733ba2
kiran1505 <kiranbk784@gmail.com>
2024-07-14 19:24:03 +0530

    1

--- Statements.py
+++ Statements.py
@@ -39,3 +39,23 @@ How old are you Kiran? 22
 You can vote
 *************************************
 
+elif statements
+
+name = input("Enter your name ")
+age = int(input("How old are you {0}? ".format(name)))
+print(age)
+
+if age<18:
+    print("You can vote")
+elif age==22:
+    print("Cast your vote. You are youth")
+else:
+    print("Please comeback after {0} years".format(18-age))
+
+
+Output:
+Enter your name kiran
+How old are you kiran? 22
+22
+Cast your vote. You are youth
+
